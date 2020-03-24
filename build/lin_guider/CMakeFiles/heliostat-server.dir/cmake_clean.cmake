27fc5cf3c2d32e776dec50f9de88f5739d053c3a
Jason Westervelt <vespira@gmail.com>
2020-03-23 17:23:39 -0700

    fixed cmake

---
+++ build/lin_guider/CMakeFiles/heliostat-server.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "heliostat-server_autogen"
+  "CMakeFiles/heliostat-server_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/heliostat-server.dir/heliostat-server_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/heliostat-server.dir/server.cpp.o"
+  "CMakeFiles/heliostat-server.dir/utils.cpp.o"
+  "heliostat-server.pdb"
+  "heliostat-server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heliostat-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/main/CMakeFiles/heliostat.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jwestervelt/build/heliostat/build/main/heliostat_autogen/mocs_compilation.cpp" "/home/jwestervelt/build/heliostat/build/main/CMakeFiles/heliostat.dir/heliostat_autogen/mocs_compilation.cpp.o"
+  "/home/jwestervelt/build/heliostat/main/image_processing.cpp" "/home/jwestervelt/build/heliostat/build/main/CMakeFiles/heliostat.dir/image_processing.cpp.o"
+  "/home/jwestervelt/build/heliostat/main/main.cpp" "/home/jwestervelt/build/heliostat/build/main/CMakeFiles/heliostat.dir/main.cpp.o"
+  "/home/jwestervelt/build/heliostat/main/ser.cpp" "/home/jwestervelt/build/heliostat/build/main/CMakeFiles/heliostat.dir/ser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "main/heliostat_autogen/include"
+  "../lin_guider"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
